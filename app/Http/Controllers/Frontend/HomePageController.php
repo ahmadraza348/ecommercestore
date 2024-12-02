ef292c0410cf2c7ec833685d01b31e11b4d05596
ahmadraza348 <engr.ahmadraza348@gmail.com>
2024-12-02 09:11:02 -0800

    Home Page Working

--- app/Http/Controllers/Frontend/HomePageController.php
+++ app/Http/Controllers/Frontend/HomePageController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Product;
 
 class HomePageController extends Controller
 {
-    public function index(){
-        $data['featured_categories'] = Category::where(['status'=>1,'is_featured'=>1 ])->with('products')->get();
+    public function index()
+    {
+        $data['featured_categories'] = Category::where(['status' => 1, 'is_featured' => 1])->with('products')->get();
+        $data['hot_deals_pro'] = Product::where(['status' => 1, 'label' => 'hot'])->take(3)->get();
         return view('frontend.index', $data);
     }
 }
