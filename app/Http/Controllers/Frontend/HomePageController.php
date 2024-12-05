f60e028848e963c5618279262f5d251da8700285
ahmadraza348 <engr.ahmadraza348@gmail.com>
2024-12-05 10:12:33 +0500

    again commit

--- app/Http/Controllers/Frontend/HomePageController.php
+++ app/Http/Controllers/Frontend/HomePageController.php
@@ -9,7 +9,6 @@ use App\Models\Product;
 
 class HomePageController extends Controller
 {
-<<<<<<< HEAD
     public function index()
     {
         $data['featured_categories'] = Category::where(['status' => 1, 'is_featured' => 1])->take(6)->with('products')->get();
@@ -17,14 +16,6 @@ class HomePageController extends Controller
         $data['hot_deals_pro'] = Product::where(['status' => 1, 'label' => 'hot'])->take(6)->get();
         $data['sale_pro'] = Product::where(['status' => 1, 'label' => 'sale'])->take(6)->get();
         $data['new_arrival_pro'] = Product::where(['status' => 1, 'label' => 'new'])->take(6)->get();
-=======
-
-    public function index()
-    {
-        $data['featured_categories'] = Category::where(['status' => 1, 'is_featured' => 1])->with('products')->get();
-        $data['featured_pro'] = Product::where(['status' => 1, 'is_featured' => 1])->take(8)->get();
-        $data['hot_deals_pro'] = Product::where(['status' => 1, 'label' => 'hot'])->take(3)->get();
->>>>>>> 9a5891377537a7dcddf040b1d66faed7a6f320d4
         return view('frontend.index', $data);
     }
 }
